a342699c86aca614a40cd64e1c4ba6c64278c29d
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-02-17 20:47:27 +0300

    Stack  TryPop

--- Program.cs
+++ Program.cs
@@ -10,29 +10,13 @@ namespace z25
             var people = new Stack<string>();
             people.Push("Tom");
             // people = { Tom }
-            people.Push("Sam");
-            // people = { Sam, Tom }
-            people.Push("Bob");
-            // people = { Bob, Sam, Tom }
 
-            // получаем первый элемент стека без его удаления 
-            string headPerson = people.Peek();
-            Console.WriteLine(headPerson);  // Bob
+            // удаляем элементы
+            var success1 = people.TryPop(out var person1);  // success1 = true
+            if (success1) Console.WriteLine(person1); // Tom
 
-            string person1 = people.Pop();
-            // people = { Sam, Tom }
-            Console.WriteLine(person1);  // Bob
-
-            string person2 = people.Pop();
-            // people = { Tom }
-            Console.WriteLine(person2);  // Sam
-
-            string person3 = people.Pop();
-            // people = { }
-            Console.WriteLine(person3);  // Tom
-
-
-
+            var success2 = people.TryPeek(out var person2);  // success2 = false
+            if (success2) Console.WriteLine(person2);
 
         }
     }
