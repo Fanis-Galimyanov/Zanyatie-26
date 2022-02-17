6768b5d5733089bfe5cbdcceae34580551e221b1
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-02-17 20:44:39 +0300

    Stack exemple 1

--- Program.cs
+++ Program.cs
@@ -7,11 +7,30 @@ namespace z25
     {
         static void Main(string[] args)
         {
-            var employees = new List<string> { "Tom", "Sam", "Bob" };
-            Stack<string> people = new Stack<string>(employees);
-            foreach (var person in people) Console.WriteLine(person);
+            var people = new Stack<string>();
+            people.Push("Tom");
+            // people = { Tom }
+            people.Push("Sam");
+            // people = { Sam, Tom }
+            people.Push("Bob");
+            // people = { Bob, Sam, Tom }
 
-            Console.WriteLine(people.Count); // 3
+            // получаем первый элемент стека без его удаления 
+            string headPerson = people.Peek();
+            Console.WriteLine(headPerson);  // Bob
+
+            string person1 = people.Pop();
+            // people = { Sam, Tom }
+            Console.WriteLine(person1);  // Bob
+
+            string person2 = people.Pop();
+            // people = { Tom }
+            Console.WriteLine(person2);  // Sam
+
+            string person3 = people.Pop();
+            // people = { }
+            Console.WriteLine(person3);  // Tom
+
 
 
 
