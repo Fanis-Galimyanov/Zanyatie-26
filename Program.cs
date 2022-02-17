f45037c1e5c497fc004599020d0272d9796062c2
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-02-17 20:06:23 +0300

    LinkedList while

--- Program.cs
+++ Program.cs
@@ -8,12 +8,24 @@ namespace z25
         static void Main(string[] args)
         {
 
-            var employees = new List<string> { "Tom", "Sam", "Bob" };
+            LinkedList<string> people = new LinkedList<string>(new[] { "Tom", "Sam", "Bob" });
 
-            LinkedList<string> people = new LinkedList<string>(employees);
-            Console.WriteLine(people.Count);            // 3
-            Console.WriteLine(people.First?.Value);    // Tom
-            Console.WriteLine(people.Last?.Value);    // Bob
+            // от начала до конца списка
+            var currentNode = people.First;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Next;
+            }
+
+            // с конца до начала списка
+            currentNode = people.Last;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Previous;
+            }
+
 
 
 
