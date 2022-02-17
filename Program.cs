82370050df71005b65cc44e52541cf71802ffbfc
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-02-17 21:07:30 +0300

    Dictionary ContainsKey

--- Program.cs
+++ Program.cs
@@ -7,22 +7,30 @@ namespace z25
     {
         static void Main(string[] args)
         {
-            var people = new Dictionary<int, string>()
-            {
-                [5] = "Tom",
-                [6] = "Sam",
-                [7] = "Bob",
-            };
-            // получаем элемент по ключу 6
-            string sam = people[6];  // Sam
-            Console.WriteLine(sam);  // Sam
-                                     // переустанавливаем значение по ключу 6
-            people[6] = "Mike";
-            Console.WriteLine(people[6]);  // Mike
+            // условная телефонная книга
+            var phoneBook = new Dictionary<string, string>();
 
-            // добавляем новый элемент по ключу 22
-            people[22] = "Eugene";
-            Console.WriteLine(people[22]);  // Eugene
+            // добавляем элемент: ключ - номер телефона, значение - имя абонента
+            phoneBook.Add("+123456", "Tom");
+            // альтернативное добавление
+            // phoneBook["+123456"] = "Tom";
+
+            // Проверка наличия
+            var phoneExists1 = phoneBook.ContainsKey("+123456");    // true
+            Console.WriteLine($"+123456: {phoneExists1}");
+            var phoneExists2 = phoneBook.ContainsKey("+567456");    // false
+            Console.WriteLine($"+567456: {phoneExists2}");
+            var abonentExists1 = phoneBook.ContainsValue("Tom");      // true
+            Console.WriteLine($"Tom: {abonentExists1}");
+            var abonentExists2 = phoneBook.ContainsValue("Bob");      // false
+            Console.WriteLine($"Bob: {abonentExists2}");
+
+            // удаление элемента
+            phoneBook.Remove("+123456");
+
+            // проверяем количество элементов после удаления
+            Console.WriteLine($"Count: {phoneBook.Count}"); // Count: 0
+
 
         }
     }
