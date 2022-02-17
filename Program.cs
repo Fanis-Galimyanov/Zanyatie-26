5cba4ad1696f56d16a9b422bc9fe4752b64106cd
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-02-17 17:52:18 +0300

    Hello

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace z25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
