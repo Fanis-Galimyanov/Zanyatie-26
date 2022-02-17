e6d9df61b5d3f98b8f2f7354a56045e2f8c5bd72
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-02-17 19:55:01 +0300

    LinkedList

--- Program.cs
+++ Program.cs
@@ -8,23 +8,14 @@ namespace z25
         static void Main(string[] args)
         {
 
-            var people = new List<string>() { "Eugene", "Tom", "Mike", "Sam", "Bob" };
+            var employees = new List<string> { "Tom", "Sam", "Bob" };
 
-            // переворачиваем весь список
-            people.Reverse();
-            // people = { "Bob","Sam", "Mike", "Tom", "Eugene"};
+            LinkedList<string> people = new LinkedList<string>(employees);
+            foreach (string person in people)
+            {
+                Console.WriteLine(person);
+            }
 
-            var people2 = new List<string>() { "Eugene", "Tom", "Mike", "Sam", "Bob" };
-            // переворачиваем часть только 3 элемента с индекса 1
-            people2.Reverse(1, 3);
-            // people2 = { "Eugene","Sam", "Mike", "Tom", "Bob" };
-
-
-
-            /* foreach (var person in partOfPeople)
-             {
-                 Console.WriteLine(person);
-             }*/
 
         }
     }
